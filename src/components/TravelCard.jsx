2c5c2312d53bc0cceb42d8c0d0736998b6a7c900
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-04-23 18:41:59 +0500

    fixed: galery section

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -12,15 +12,15 @@ function TravelCard({
 }) {
   return (
     <div
-      className="md:w-[450px] w-full h-auto rounded-xl overflow-hidden border-1"
+      className="relative md:w-[450px] w-full md:h-[600px] h-[520px] rounded-xl overflow-hidden border-1"
       key={index}
     >
       <img
         src={src}
         alt={src}
-        className="w-full  object-cover  md:h-[400px] h-[300px]"
+        className="w-full  object-cover  md:h-[300px] h-[250px]"
       />
-      <div className="p-5">
+      <div className="p-5 relative">
         <h1 className="text-xl font-medium">{title}</h1>
         <div className="my-5 flex flex-col gap-2">
           <div className="flex justify-between items-center">
@@ -47,11 +47,10 @@ function TravelCard({
             <span className="text-black/70">{dates}</span>
           </div>
         </div>
-
-        <div className="flex justify-between font-medium text-xl">
-          <span>Price:</span>
-          <span>{price}</span>
-        </div>
+      </div>
+      <div className="flex w-full px-6 items-center justify-between font-medium text-xl absolute left-0 bottom-6">
+        <span>Price:</span>
+        <span>{price}</span>
       </div>
     </div>
   );
